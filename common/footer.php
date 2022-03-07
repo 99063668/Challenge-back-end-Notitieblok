e74981bf79c42de804486ece30f8cbe682aaeb1e
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-07 17:15:50 +0100

    styling header + footer

---
+++ common/footer.php
@@ -0,0 +1,5 @@
+<body class="d-flex flex-column min-vh-100">
+<br>
+<footer class="bg-dark text-center text-white mt-auto">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">©2022 Dewi van der Velden</div>
+</footer>
---
+++ common/header.php
@@ -0,0 +1,11 @@
+<head>
+  <title>Index</title>
+  <link rel="stylesheet" href="../style.css">
+  <script src="https://kit.fontawesome.com/198c4d6390.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1"><i class="fa-solid fa-note-sticky"></i><a href="script/makeList.php" class="navbar">Lijst maken</a></span>
+    </nav>
+    
