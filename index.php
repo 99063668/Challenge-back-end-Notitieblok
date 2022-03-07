f016aac90040ed68e6282bb76f89b9a28f0c61c5
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-07 11:00:27 +0100

    overzicht toegevoegd

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
 <?php
+  include("script/functions.php");
   $notes = getAllNotes();
+  $lists = getAllList();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Index</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+  <body>
+    <input type="button" value="Lijst maken" id="btnClose" onClick="Javascript:window.location.href = 'script/makeList.php';" />
+    <br>
+    <br>
 
-<input type="button" value="Lijst maken" id="btnClose" onClick="Javascript:window.location.href = 'script/makeList.php';" />
-<input type="button" value="Notitie maken" id="btnClose" onClick="Javascript:window.location.href = 'script/makeNote.php';" />
+    <table>
+      <?php
+        foreach($lists as $list){
+      ?>
+        <tr>
+          <th><?=$list["title"]?></th>
+        </tr>
+      <?php
+        }
+      ?>
 
-<tbody>
-  <?php
-    foreach($notes as $note){
-  ?>
-  <tr>
-    <td><?=$note["title"]?></td>
-    <td><?=$note["task"]?></td>
-    <td><?=$note["description"]?></td>
-    <td><?=$note["duration"]?></td>
-  </tr>
-  <?php
-    }
-  ?>
-</tbody>
+
+      <?php
+        foreach($notes as $note){
+      ?>
+      <tr>
+        <td><a href="script/detail.php?id=<?=$note["id"]?>"><?=$note["title"]?></a></td>
+        <td><?=$note["task"]?></td>
+      <?php
+        }
+      ?>
+      <br>
+      <Td><input type="button" value="Notitie maken" id="btnClose" onClick="Javascript:window.location.href = 'script/makeNote.php';" /></Td>
+    </table>
+</body>
+</html>
