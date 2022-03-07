2c930d20716f8326b7135dd80ba8429cb06a7274
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-07 17:16:09 +0100

    wijzigingen styling

--- index.php
+++ index.php
@@ -3,47 +3,56 @@
   $notes = getAllNotes();
   $lists = getAllList();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Index</title>
   <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/198c4d6390.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
   <body>
+  <header>
     <nav class="navbar navbar-dark bg-dark">
-      <span class="navbar-brand mb-0 h1"> <input type="button" value="Lijst maken" id="btnClose" onClick="Javascript:window.location.href = 'script/makeList.php';" /></span>
+      <span class="navbar-brand mb-0 h1"><i class="fa-solid fa-note-sticky"></i><a href="script/makeList.php" class="navbar">Lijst maken</a></span>
     </nav>
+  </header>
 
-    <?php
-      foreach($lists as $list){
-    ?>
-    <table>
-      </tr>
-        <th><?=$list["title2"]?></th>
-      </tr>
+  <?php
+    foreach($lists as $list){
+  ?>
+  <div class="row">
+    <div class="column">
+      <table>
+        </tr>
+          <th><?=$list["title2"]?></th>
+        </tr>
 
-      <?php
-        foreach($notes as $note){
-          if($note["listId"] == $list["id"]){
-      ?>
-      <tr>
-        <td><a class="link" href="script/detail.php?id=<?=$note["id"]?>"><?=$note["title"]?></a></td>
-      <?php
+        <?php
+          foreach($notes as $note){
+            if($note["listId"] == $list["id"]){
+        ?>
+        <tr>
+          <td><a class="link" href="script/detail.php?id=<?=$note["id"]?>"><?=$note["title"]?></a></td>
+        <?php
+            }
           }
+        ?>
+
+        </tr>
+        <br>
+        <td id="indexBtn"><a href="script/makeNote.php?listId=<?=$list["id"]?>" class="buttonsList">Notitie maken</a></td>
+      </table>
+      <?php
         }
       ?>
-
-      </tr>
-      <br>
-      <td><a href="script/makeNote.php?listId=<?=$list["id"]?>" class="buttons">Notitie maken</a></td>
-    </table>
-    <?php
-      }
-    ?>
-  </body>
+    </div>
+  </div>
 </html>
 
-<?php   
-  include("common/footer.php"); 
-?>
+<footer>
+  <?php   
+    include("common/footer.php"); 
+  ?>
+</footer>
