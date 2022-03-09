fa868e8bbe507e81fbbc86aa6bd3a77fc7a589c5
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-09 18:55:47 +0100

    header aangepast

--- common/header.php
+++ common/header.php
@@ -4,8 +4,9 @@
   <script src="https://kit.fontawesome.com/198c4d6390.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-  <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <span class="navbar-brand mb-0 h1"><i class="fa-solid fa-note-sticky"></i><a href="script/makeList.php" class="navbar">Lijst maken</a></span>
-    </nav>
-    
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand"><i class="fa-solid fa-note-sticky" style="padding-right: 4px;"></i>Notities</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
