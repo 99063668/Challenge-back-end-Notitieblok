bb511b4f7e968d181d669ff03a9bacc065bf5006
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-04-04 10:42:57 +0200

    database update

--- script/notities.sql
+++ script/notities.sql
@@ -3,14 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Gegenereerd op: 04 apr 2022 om 08:12
--- Serverversie: 8.0.28
--- PHP-versie: 7.4.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Generation Time: Apr 04, 2022 at 08:33 AM
+-- Server version: 8.0.28
+-- PHP Version: 7.4.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,63 +15,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `notities`
 --
+CREATE DATABASE IF NOT EXISTS `notities`;
+USE `notities`;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `list`
+-- Table structure for table `list`
 --
 
-CREATE TABLE `list` (
-  `id` int NOT NULL,
-  `title2` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+CREATE TABLE IF NOT EXISTS `list` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title2` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tabelstructuur voor tabel `todolist`
+-- Table structure for table `todolist`
 --
 
-CREATE TABLE `todolist` (
-  `id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `todolist` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `task` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `duration` int DEFAULT NULL
+  `duration` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `list`
---
-ALTER TABLE `list`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `todolist`
---
-ALTER TABLE `todolist`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `list`
---
-ALTER TABLE `list`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT voor een tabel `todolist`
---
-ALTER TABLE `todolist`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
