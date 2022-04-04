ed5007214a9432c461ae9318763a3969e3d4414c
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-04-04 11:21:41 +0200

    db update

--- script/notities.sql
+++ script/notities.sql
@@ -3,9 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 04, 2022 at 08:33 AM
+-- Generation Time: Apr 04, 2022 at 09:21 AM
 -- Server version: 8.0.28
 -- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,8 +20,6 @@
 --
 -- Database: `notities`
 --
-CREATE DATABASE IF NOT EXISTS `notities`;
-USE `notities`;
 
 -- --------------------------------------------------------
 
@@ -24,10 +27,9 @@ USE `notities`;
 -- Table structure for table `list`
 --
 
-CREATE TABLE IF NOT EXISTS `list` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title2` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `list` (
+  `id` int NOT NULL,
+  `title2` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -36,14 +38,46 @@ CREATE TABLE IF NOT EXISTS `list` (
 -- Table structure for table `todolist`
 --
 
-CREATE TABLE IF NOT EXISTS `todolist` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `todolist` (
+  `id` int NOT NULL,
   `title` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `task` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `status` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `duration` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `listId` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `list`
+--
+ALTER TABLE `list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `todolist`
+--
+ALTER TABLE `todolist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `list`
+--
+ALTER TABLE `list`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `todolist`
+--
+ALTER TABLE `todolist`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
