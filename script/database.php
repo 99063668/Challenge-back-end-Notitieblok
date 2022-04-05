0ef0fda95aa8f8fa67434283619e14d352feb4eb
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-04-05 10:16:55 +0200

    db update

--- script/database.php
+++ script/database.php
@@ -1,15 +1,26 @@
 <?php
 function openDatabase(){
-  $host = "localhost";
-  $username = "root";
-  $password = "mysql";
-  $database = "notities";
+  // $host = "localhost";
+  // $username = "root";
+  // $password = "mysql";
+  // $database = "notities";
+
+  $host = "bnrnjl4vfunlw6rzlb8q-mysql.services.clever-cloud.com";
+  $database = "bnrnjl4vfunlw6rzlb8q";
+  $username = "ueyaw8tc2soo8mhh";
+  $password = "eQzxlIgt0Om3elFTENb5";
 
   try {
-    $connect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    // $connect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $connect = new PDO(
+    	"mysql:host=" . getenv("MYSQL_ADDON_HOST") . ";dbname=" . getenv("MYSQL_ADDON_DB"),
+    	getenv("MYSQL_ADDON_USER"),
+    	getenv("MYSQL_ADDON_PASSWORD")
+  );
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $connect;
   } catch (PDOException $error) {
+    die("kan geen verbiding maken met de db");
   }
 }
 ?>
